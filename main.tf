c6a795982a79a4b6df3cd5d5a23897a4026539e9
Adekunle Adedeji <ceeepath@gmail.com>
2023-11-26 09:53:29 +0000

    aws vpc resource block created

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "vpc" {
-  source = "./modules/vpc"
+  source      = "./modules/vpc"
   vpc_configs = var.vpc_configs
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -5,13 +5,13 @@ variable "vpc_configs" {
   enable_dns_hostnames = (Optional) A boolean flag to enable/disable DNS hostnames in the VPC. Defaults to false.
   DESCRIPTION
   type = object({
-    cidr_block = string
-    instance_tenancy = string
-    enable_dns_hostnames = bool 
+    cidr_block           = string
+    instance_tenancy     = string
+    enable_dns_hostnames = bool
   })
   default = {
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
+    cidr_block           = "10.0.0.0/16"
+    instance_tenancy     = "default"
     enable_dns_hostnames = false
   }
 }
